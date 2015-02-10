ee3f75e62bd809bee279077cbe8603cc8078e0cc
Vasanth <vasanth@solitontech.com>
2015-02-10 19:38:04 +0530

    yet to verify its working in actual proj settings

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var mocha = require('mocha');
 	glob = require('glob'),
  	cp = require('child_process'),
  	fs = require('fs'),
- 	path = require('path');
+ 	path = require('path'),
+ 	q = require('q');
 
 
-function run() {
-	var args = process.argv;
-	var config = "nwtest.config.json";
+(function() {
+	var args = process.argv,
+	    config = "nwtest.config.json";
 
 
 	if(fs.existsSync(config)) {
@@ -16,24 +17,54 @@ function run() {
 		console.log("nw-test-runner needs the config file to be present in the directory in which tests are executed. Please see readme for more details");
 		return;
 	}
-	//This check not needed
-	if(config) {
-		try {
-			var aconf = path.resolve(config);
-			var opts = require(aconf);
+
+	try {
+			var aconf = path.resolve(config), 
+				opts = require(aconf),
+				fse = require('fs-extra'),
+				output_folder = "",
+				nw_path = "",
+				i = 0,
+				src, 
+				mock, 
+				test;
 			
 			if(opts.src) {
-				var src = glob.sync(opts.src);
+				src = glob.sync(opts.src);
 			}
 			if(opts.mock) {
-				var mock = glob.sync(opts.mock);
+				mock = glob.sync(opts.mock);
 			}
 			if(opts.test) {
-				var test = glob.sync(opts.test);
+				test = glob.sync(opts.test);
+			} else { 
+				console.log("Test files to execute should be mentioned in config file")
+				return;
 			}
-			for(var k in test) {
-				var file = test[k];
 
+
+			
+			if(!opts.output) {
+				output_folder = "nwtest_results";
+
+			} else {
+				output_folder = opts.output;
+			}
+			fse.ensureDirSync(output_folder);
+
+			//Now get the node path
+			if(!opts.nwpath) {
+				nw_path = '"node_modules/nodewebkit/nodewebkit/nw"';
+			} else {
+				nw_path = opts.nwpath;
+			}
+
+
+			var runTest = function(index) {
+
+				var defer = q.defer();
+
+				var file = test[index];
 				//Now pick a test file and find the corresponding mock and src file
 				var basename = path.basename(file);
 				var srcFileName = getBaseName(basename) + '.js';
@@ -72,43 +103,50 @@ function run() {
 				//Push the test file atlast
 				list.push(path.resolve(file));
 
-				var output_folder = "";
-				var fse = require('fs-extra');
-				if(!opts.output) {
-
-					output_folder = "nwtest_results";
-
-				} else {
-					output_folder = opts.output;
-				}
-				//fse.removeSync(output_folder);
-				fse.ensureDirSync(output_folder);
 				console.log("Running " + basename + " tests...");
 				try {
 					
-					var ls = cp.execSync('"node_modules/nodewebkit/nodewebkit/nw" ./node_modules/nw-test-runner/lib/ ' + list + ' ' + output_folder + ' ' + outputFilename);
+					var execCmd = nw_path + ' ./node_modules/nw-test-runner/lib/ ' + list + ' ' + output_folder + ' ' + outputFilename;
+					console.log(execCmd);
+					var ls = cp.exec(execCmd, function(err, done) {
+						if(err)
+							defer.reject(err);
+						else
+							defer.resolve(true);
+					});
 					
-					//Add an error check
+				//Add an error check
 				} catch(exp) {
 					console.log(exp);
+					defer.reject(exp);
 				}
-			}
+
+				return defer.promise;
+			};
+
+			runTest(i).then(function() {
+				i++
+				if(i < test.length)
+					runTest(i);
+				else
+					console.log("All tests done!");
+			}, function(err) {
+				console.log("Running test failed with error:- " + err);
+				i++;
+				if(i < test.length)
+					runTest(i);
+				else
+					console.log("All tests done!");
+			});
 			
 		} catch(exp) {
 			
 			console.log(exp);
 		}
-		
-
-	} else {
-
-	}
-}
+})();
 
 function getBaseName(filename) {
 	var name = filename.split('.test');
 	return name[0];
 }
 
-run();
-module.exports = run;
